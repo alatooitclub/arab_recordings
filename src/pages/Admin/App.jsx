7fe96d68bd53a5dca7a9301c661e84135a53d181
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2024-05-13 06:57:41 +0600

    fixed login button

--- src/pages/Admin/App.jsx
+++ src/pages/Admin/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { AuthProvider } from './contexts/AuthContext';
-import AdminPage from './components/AdminPage'; // Your admin page component
+import Admin from './pages/mainPageAdmin/Admin';
 
 function App() {
   return (
