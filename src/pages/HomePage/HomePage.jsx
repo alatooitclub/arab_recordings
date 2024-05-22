1887765a9e50c271a1fdc9ab1a7754974c36a3e8
Alieva Nargiza <nargiza.alieva018@gmail.com>
2024-05-23 02:58:29 +0600

    Make home page adoptive and add multylanguage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,37 @@
 import classes from './HomePage.module.css'
+import { useLang } from "../../hooks/useLang";
+import { setLang } from "../../contexts/lang";
 import img1 from './../../assets/hp-img1.png'
 import img2 from './../../assets/hp-img2.png'
 import img3 from './../../assets/hp-img3.png'
+import { NavLink } from 'react-router-dom'
 
 const HomePage = () => {
+    const { lang, translations } = useLang();
+
+    const handleSwitchLang = (lang) => {
+        setLang(lang);
+        localStorage.setItem("lang", JSON.stringify(lang));
+    };
+
     return ( 
         <div className={classes.homePage}>
             <div className={classes.container}> 
                 <div className={classes.text}>
-                    <div className={classes.upTitle}>Welcome to Arabic Easy</div>
-                    <div className={classes.title}>Learn language with Expert Guidance!</div>
-                    <div className={classes.subTitle}>Receive personalized feedback and tailored learning plans that adapt to your progress and learning style.</div>
+                    <div className={classes.upTitle}>{translations[lang].home_page.welcome}</div>
+                    <div className={classes.title}>{translations[lang].home_page.title}</div>
+                    <div className={classes.subTitle}>{translations[lang].home_page.subtitle}</div>
                     <div className={classes.home_btn}>
-                        <button>Discover More</button>
+                        <button>
+                            <NavLink to="/login" style={{ textDecoration: "none"}}>{translations[lang].home_page.login}</NavLink>
+                        </button>
+                        <button>
+                            <NavLink to="/register" style={{ textDecoration: "none"}}>{translations[lang].home_page.regist}</NavLink>
+                        </button>
                     </div>
                 </div>
                 <div className={classes.image}>
-                    <div >
+                    <div className={classes.image-img1}>
                         <img src={img1} alt="" />
                     </div>
                     <div>
